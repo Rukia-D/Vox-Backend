7220e34eeb5c4c93c576691a7c034496e05e6f5c
Rukia-D <97436332+Rukia-D@users.noreply.github.com>
2024-09-03 23:34:44 +0530

    fix 2

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { login, register, resetPassword, forgotPassword } = require('../controllers/auth')
+const { login, register } = require('../controllers/auth')
 const { officerLogin } = require('../controllers/officer-auth')
 const { adminLogin } = require('../controllers/admin-auth')
 
 router.route('/login').post(login)
 router.route('/register').post(register)
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
 
 router.route('/officer/login').post(officerLogin)
 
